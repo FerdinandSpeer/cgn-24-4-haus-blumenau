79a81741308db2c583b8d35529adb313e1ad0fa5
ferdinandspeer <ferdinandspeer@gmail.com>
2024-10-18 16:46:08 +0200

    add route for AdminDashboard.tsx in NavBar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import {Route, Routes} from "react-router-dom";
 import HomePage from "./pages/homePage/HomePage.tsx";
 import BookingPage from "./pages/bookingPage/BookingPage.tsx";
+import AdminDashboard from "./pages/adminDashboard/AdminDashboard.tsx";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Routes>
             <Route path={"/"} element={<HomePage/>}/>
             <Route path={"/bookingPage"} element={<BookingPage/>}/>
+            <Route path={"/adminDashboard"} element={<AdminDashboard/>}/>
         </Routes>
         </>
     )
