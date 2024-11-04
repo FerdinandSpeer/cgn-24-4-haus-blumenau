88cf04d0f9ae248e0d7880e601d78e8252dfb0c3
ferdinandspeer <ferdinandspeer@gmail.com>
2024-11-04 15:15:24 +0100

    add basic CSS for page structure

--- frontend/src/pages/guestDashboard/GuestGroupDashboard.tsx
+++ frontend/src/pages/guestDashboard/GuestGroupDashboard.tsx
@@ -6,8 +6,26 @@ export default function GuestGroupDashboard() {
     return (
         <>
             <NavBar/>
-            <h1>Guest-Group Dashboard</h1>
+            <h1>Hallo groupName Gruppe</h1>
+            <div className={"bodyGuestGroupDashboard"}>
+                <div className={"list"}>
+                    <ol className={"guestGroupList"}>
+                        <h2>Gästeliste</h2>
+                        <li> Hans Hubert <button>Bearbeiten</button></li>
+                    </ol>
+                </div>
+                <div className={"bodyWeatherBookingTime"}>
+                    <div className={"weatherApi"}>
+                        <h2>Wetter Schönau am Königssee</h2>
+                        <p>Hier zeigt die api die Daten vom Wetter an.</p>
+                    </div>
 
+                    <div className={"bookingTime"}>
+                        <h2>Buchungszeitraum</h2>
+                        <p> arrivalDate - departureDate</p>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
